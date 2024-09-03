ad4cfc2f6e0bdc360eab5b4bcefa8064dd09f9b1
jcant0n <javiercantonferrero@gmail.com>
2024-09-03 22:44:59 +0200

    Added the github workflows.

--- Generate-NuGets.ps1
+++ Generate-NuGets.ps1
@@ -1,8 +1,8 @@
 <#
 .SYNOPSIS
-	Evergine bindings NuGet Packages generator script, (c) 2022 Javier Cantón Ferrero
+	Evergine bindings NuGet Packages generator script, (c) 2024 Evergine Team
 .DESCRIPTION
-	This script generates NuGet packages for Evergine OpenGL (and OpenGL ES) bindings.
+	This script generates NuGet packages for the low-level Vulkan bindings used in Evergine
 	It's meant to have the same behavior when executed locally as when it's executed in a CI pipeline.
 .EXAMPLE
 	<script> -version 2021.11.17.1-local
@@ -13,25 +13,14 @@
 param (
 	[Parameter(mandatory=$true)][string]$version,
 	[string]$outputFolderBase = "nupkgs",
-	[string]$buildVerbosity = "minimal",
+	[string]$buildVerbosity = "normal",
 	[string]$buildConfiguration = "Release",
-	[string]$openGLcsprojPath = "OpenGLGen\Evergine.Bindings.OpenGL\Evergine.Bindings.OpenGL.csproj",
-	[string]$openGLEScsprojPath = "OpenGLGen\Evergine.Bindings.OpenGLES\Evergine.Bindings.OpenGLES.csproj"
+	[string]$vulkanBindingsCsprojPath = "OpenGLGen\Evergine.Bindings.OpenGL\Evergine.Bindings.OpenGL.csproj"
 )
 
 # Utility functions
 function LogDebug($line)
 { Write-Host "##[debug] $line" -Foreground Blue -Background Black
-}
-
-# Locate build tools
-$msbuildPath = tools\vswhere.exe -latest -prerelease -products * -requires Microsoft.Component.MSBuild -property installationPath
-if(-Not $?)
-{ exit $lastexitcode
-}
-$msbuildPath = Resolve-Path (Join-Path $msbuildPath 'MSBuild\*\Bin\MSBuild.exe')
-if (-Not (Test-Path $msbuildPath))
-{ throw "MSBuild not found."
 }
 
 # Show variables
@@ -43,24 +32,26 @@ LogDebug "Output folder.......: $outputFolderBase"
 LogDebug "#######################################"
 
 # Create output folder
-$outputFolder = Join-Path $outputFolderBase $versionWithSuffix
+$outputFolder = Join-Path $outputFolderBase $version
 New-Item -ItemType Directory -Force -Path $outputFolder
 $absoluteOutputFolder = Resolve-Path $outputFolder
 
-# Create temporal solution
-$tempSolutionName = "tempsolution_deleteme"
-Remove-Item "$tempSolutionName.sln" -ErrorAction Ignore
-dotnet new sln -n "$tempSolutionName"
-
-# Add projects to the solution
-dotnet sln "$tempSolutionName.sln" add $openGLcsprojPath
-dotnet sln "$tempSolutionName.sln" add $openGLEScsprojPath
+$symbols = false
+if($buildConfiguration -eq "Debug")
+{
+	$symbols = true
+}
 
 # Generate packages
 LogDebug "START packaging process"
-& $msbuildPath /nologo "$tempSolutionName.sln" /t:restore,build,pack /p:Configuration=$buildConfiguration /v:$buildVerbosity /p:PackageOutputPath="$absoluteOutputFolder" /p:IncludeSymbols=true /p:Version=$version
+dotnet pack "$vulkanBindingsCsprojPath" -v:$buildVerbosity -p:Configuration=$buildConfiguration -p:PackageOutputPath="$absoluteOutputFolder" -p:IncludeSymbols=$symbols -p:Version=$version
+if($?)
+{
+   LogDebug "END packaging process"
+}
+else
+{
+	LogDebug "ERROR; dotnet pack failed"
+   	exit -1
+}
 
-# Post-script tasks
-Remove-Item "$tempSolutionName.sln" -ErrorAction Ignore
-
-LogDebug "END packaging process"
